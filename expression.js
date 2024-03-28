5d41cf79625089407635611adaf290f55b6d52c0
anmol1gfs121 <92971089+anmol1gfs121@users.noreply.github.com>
2024-03-28 19:25:43 +0530

    Add files via upload

---
+++ expression.js
@@ -0,0 +1,15 @@
+/*Problem 2: Play with Expressions
+Create a program that calculates the result of the following expression: 
+(a + b) * c / d - e
+Take the above variables a, b, c, d, and e and store the value according to you 
+then store the result of the above expression in a separate variable and print 
+the result using console.log().*/
+
+var a,b,c,d,e,n;
+a=4;
+b=5;
+c=6;
+d=2;
+e=9;
+n=(a + b) * c / d - e;
+console.log("The result of the abobe expression is : ",n)
