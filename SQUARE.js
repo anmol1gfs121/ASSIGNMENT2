aae716b5cbe53505c07fb528362dd5c6c01e9473
anmol1gfs121 <92971089+anmol1gfs121@users.noreply.github.com>
2024-03-28 19:19:54 +0530

    Add files via upload

---
+++ SQUARE.js
@@ -0,0 +1,7 @@
+/*Problem 1: Calculate the Square of a Number
+Write a JavaScript program to calculate and print the square of a given number.*/
+var n=parseInt(prompt("Enter a number : "));
+var m;
+m=n*n;
+console.log("The square of the given number is : ",m);
+
