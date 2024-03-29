a2e3b9f0106b3ed7a5bc9853808f67bcd990a1ab
anmol1gfs121 <92971089+anmol1gfs121@users.noreply.github.com>
2024-03-29 09:52:19 +0530

    Add files via upload

---
+++ multipleof5.js
@@ -0,0 +1,10 @@
+/*Problem 6: Multiple of 5
+Take a variable, store a number, and check if a number is a multiple of 5.*/
+
+var n=parseInt(prompt("Enter Any Number : "));
+if(n%5==0){
+    console.log("Given Number Is A multiple Of Five")
+}
+else{
+    console.log("Given Number Is Not A multiple Of Five")
+}
