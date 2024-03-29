30459de1dae3d5955ddcfa4f4e3903c991e8c9d9
anmol1gfs121 <92971089+anmol1gfs121@users.noreply.github.com>
2024-03-29 09:46:16 +0530

    Add files via upload

---
+++ rectangle.js
@@ -0,0 +1,9 @@
+/*Problem 5: Rectangle
+ Write a JavaScript expression to calculate the area of a rectangle with a length 
+of 8 units and a width of 5 units.*/
+var L,B,A;
+L=parseInt(prompt("Enter the length of rectangle : "));
+B=parseInt(prompt("Enter the width of rectangle : "))
+A=L*B;
+console.log("Area of Rectangle Is : ",A);
+
